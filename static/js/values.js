46537523665a32ce281a19eefe68ca76f194e046
Benjamin Tsai <hello@benjamintsai.com>
2014-04-25 18:08:53 -0700

    Translate into Traditional Chinese for Taiwan based on Simplified Chinese version.

--- static/js/values.js
+++ static/js/values.js
@@ -9,23 +9,23 @@ angular.module('tour.values', []).
 // List of modules with description and lessons in it.
 value('TableOfContents', [{
     'id': 'mechanics',
-    'title': 'Using the tour',
-    'description': '<p>Welcome to a tour of the <a href="http://golang.org">Go programming language</a>. The tour covers the most important features of the language, mainly:</p>',
+    'title': '如何使用本指南',
+    'description': '<p>歡迎來到<a href="http://golang.org/">Go程式語言</a>指南。本指南涵蓋Go語言的主要功能。</p>',
     'lessons': ['welcome']
 }, {
     'id': 'basics',
-    'title': 'Basics',
-    'description': '<p>The starting point, learn all the basics of the language.</p><p>Declaring variables, calling functions, and all the things you need to know before moving to the next lessons.</p>',
+    'title': '基礎',
+    'description': '<p>出發點，學習語言的基礎。</p><p>如何定義新的變數，執行函式，及任何接下來模組必要的基礎知識。</p>',
     'lessons': ['basics', 'flowcontrol', 'moretypes']
 }, {
     'id': 'methods',
-    'title': 'Methods and interfaces',
-    'description': '<p>Learn how to define methods on types, how to declare interfaces, and how to put everything together.</p>',
+    'title': '方法和介面',
+    'description': '<p>學習如何在類型定義方法，如何定義介面，及如何合併起來。</p>',
     'lessons': ['methods']
 }, {
     'id': 'concurrency',
-    'title': 'Concurrency',
-    'description': '<p>Go provides concurrency features as part of the core language.</p><p>This module goes over goroutines and channels, and how they are used to implement different concurrency patterns.</p>',
+    'title': '並行性',
+    'description': '<p>Go的語言本身已提供並行性功能。</p><p>本模組會介紹goroutine和channels跟如何在不同的並行設計模式使用。</p>',
     'lessons': ['concurrency']
 }]).
 
